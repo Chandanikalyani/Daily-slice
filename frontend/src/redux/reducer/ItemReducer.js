c1a14a80f014cb29067c8c23a348368882f5ac18
Chandanikalyani <chandanikalyani9@gmail.com>
2024-05-25 11:22:11 +0530

    set item,offer,package Reducer

--- frontend/src/redux/reducer/ItemReducer.js
+++ frontend/src/redux/reducer/ItemReducer.js
@@ -1,87 +1,30 @@
 // ItemReducer.js
 
-const {
-    CREATE_ITEM_REQUEST,
-    CREATE_ITEM_SUCCESS,
-    CREATE_ITEM_FAILURE,
-    GET_ALL_ITEMS_REQUEST,
-    GET_ALL_ITEMS_SUCCESS,
-    GET_ALL_ITEMS_FAILURE,
-    GET_ITEM_BY_ID_REQUEST,
-    GET_ITEM_BY_ID_SUCCESS,
-    GET_ITEM_BY_ID_FAILURE,
-    UPDATE_ITEM_REQUEST,
-    UPDATE_ITEM_SUCCESS,
-    UPDATE_ITEM_FAILURE,
-    DELETE_ITEM_REQUEST,
-    DELETE_ITEM_SUCCESS,
-    DELETE_ITEM_FAILURE,
-  } = require('./ItemConstants');
-  
-  const initialState = {
-    items: [],
-    loading: false,
-    error: null,
-  };
-  
-  const ItemReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case CREATE_ITEM_REQUEST:
-      case GET_ALL_ITEMS_REQUEST:
-      case GET_ITEM_BY_ID_REQUEST:
-      case UPDATE_ITEM_REQUEST:
-      case DELETE_ITEM_REQUEST:
-        return {
-          ...state,
-          loading: true,
-          error: null,
-        };
-      case CREATE_ITEM_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          items: [...state.items, action.payload],
-        };
-      case GET_ALL_ITEMS_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          items: action.payload,
-        };
-      case GET_ITEM_BY_ID_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          items: state.items.find((item) => item.id === action.payload),
-        };
-      case UPDATE_ITEM_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          items: state.items.map((item) =>
-            item.id === action.payload.id ? action.payload : item
-          ),
-        };
-      case DELETE_ITEM_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          items: state.items.filter((item) => item.id !== action.payload),
-        };
-      case CREATE_ITEM_FAILURE:
-      case GET_ALL_ITEMS_FAILURE:
-      case GET_ITEM_BY_ID_FAILURE:
-      case UPDATE_ITEM_FAILURE:
-      case DELETE_ITEM_FAILURE:
-        return {
-          ...state,
-          loading: false,
-          error: action.payload,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  module.exports = ItemReducer;
-  
+import {
+  ITEM_CREATE_REQUEST,
+  ITEM_CREATE_SUCCESS,
+  ITEM_CREATE_FAIL
+
+} from '../constants/ItemConstants';
+
+// Initial state
+const initialState = {
+  loading: false,
+  items: [],
+  error: null,
+};
+
+// Reducer function
+export const itemReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ITEM_CREATE_REQUEST:
+      return { ...state, loading: true };
+    case ITEM_CREATE_SUCCESS:
+      return { ...state, loading: false, items: [...state.items, action.payload] };
+    case ITEM_CREATE_FAIL:
+      return { ...state, loading: false, error: action.payload };
+    // Other cases for list, details, update, delete...
+    default:
+      return state;
+  }
+};
--- frontend/src/redux/reducer/OfferReducer.js
+++ frontend/src/redux/reducer/OfferReducer.js
@@ -1,87 +1,26 @@
-// OfferReducer.js
+import {
+  OFFER_CREATE_REQUEST,
+  OFFER_CREATE_SUCCESS,
+  OFFER_CREATE_FAIL
+} from '../constants/OfferConstants';
 
-const {
-    CREATE_OFFER_REQUEST,
-    CREATE_OFFER_SUCCESS,
-    CREATE_OFFER_FAILURE,
-    GET_ALL_OFFERS_REQUEST,
-    GET_ALL_OFFERS_SUCCESS,
-    GET_ALL_OFFERS_FAILURE,
-    GET_OFFER_BY_ID_REQUEST,
-    GET_OFFER_BY_ID_SUCCESS,
-    GET_OFFER_BY_ID_FAILURE,
-    UPDATE_OFFER_REQUEST,
-    UPDATE_OFFER_SUCCESS,
-    UPDATE_OFFER_FAILURE,
-    DELETE_OFFER_REQUEST,
-    DELETE_OFFER_SUCCESS,
-    DELETE_OFFER_FAILURE,
-  } = require('./OfferConstants');
-  
-  const initialState = {
-    offers: [],
-    loading: false,
-    error: null,
-  };
-  
-  const OfferReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case CREATE_OFFER_REQUEST:
-      case GET_ALL_OFFERS_REQUEST:
-      case GET_OFFER_BY_ID_REQUEST:
-      case UPDATE_OFFER_REQUEST:
-      case DELETE_OFFER_REQUEST:
-        return {
-          ...state,
-          loading: true,
-          error: null,
-        };
-      case CREATE_OFFER_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          offers: [...state.offers, action.payload],
-        };
-      case GET_ALL_OFFERS_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          offers: action.payload,
-        };
-      case GET_OFFER_BY_ID_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          offers: state.offers.find((offer) => offer.id === action.payload),
-        };
-      case UPDATE_OFFER_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          offers: state.offers.map((offer) =>
-            offer.id === action.payload.id ? action.payload : offer
-          ),
-        };
-      case DELETE_OFFER_SUCCESS:
-        return {
-          ...state,
-          loading: false,
-          offers: state.offers.filter((offer) => offer.id !== action.payload),
-        };
-      case CREATE_OFFER_FAILURE:
-      case GET_ALL_OFFERS_FAILURE:
-      case GET_OFFER_BY_ID_FAILURE:
-      case UPDATE_OFFER_FAILURE:
-      case DELETE_OFFER_FAILURE:
-        return {
-          ...state,
-          loading: false,
-          error: action.payload,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  module.exports = OfferReducer;
-  
+// Initial state
+const initialState = {
+  loading: false,
+  offer: null,
+  error: null,
+};
+
+// Reducer function
+export const offerReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case OFFER_CREATE_REQUEST:
+      return { ...state, loading: true };
+    case OFFER_CREATE_SUCCESS:
+      return { ...state, loading: false, offer: action.payload };
+    case OFFER_CREATE_FAIL:
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
